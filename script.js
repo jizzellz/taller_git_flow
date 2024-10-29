14a8091a468be9a6cf6f5267c48aae3d52e99519
jizzellz <lesly.z@hotmail.com>
2024-10-28 22:33:31 -0500

    Creando nueva rama, para realizar fusion de cambios

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//*Este es un metodo
+//*Este es un metodo , sigueme para mas informacion
 document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
